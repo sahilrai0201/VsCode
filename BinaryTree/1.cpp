5a57a615c717fd968848b18b2f6f9a03e675408e
sahilrai0201 <sahilatwork21@gmail.com>
2026-02-15 18:15:33 +0530

    first commit

---
+++ BinaryTree/1.cpp
@@ -0,0 +1,49 @@
+//CREATION OF BINARY TREE
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+
+    //constructor
+    node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }    
+};
+
+//function to create tree
+node *buildTree(node *root){
+
+    cout << "Enter the data : " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if(data == -1){
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left :" << " " << data << endl;
+    root->left = buildTree(root->left);
+
+    cout << "Enter data for inserting in right :" << " " << data << endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+int main(){
+
+    node *root = NULL;
+
+    //ceating a tree
+    root = buildTree(root);
+
+    return 0;
+}
