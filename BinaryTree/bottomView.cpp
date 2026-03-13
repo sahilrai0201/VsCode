79c99fa751ceb7f3eef7f28078eb8c04329f461c
sahilrai0201 <sahilatwork21@gmail.com>
2026-03-13 10:54:08 +0530

    first commit

---
+++ BinaryTree/bottomView.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+vector<int> bottomView(Node* root){
+    vector<int> ans;
+    if(root == NULL) return ans;
+
+    map<int, int> topNode;
+    queue<pair<Node*, int>> q;
+
+    q.push(make_pair(root, 0));
+
+    while(!q.empty()){
+        pair<Node*, int> temp = q.front();
+        q.pop();
+
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+
+        //one to one mapping
+        // if(topNode.find(hd) == topNode.end()){
+        //     topNode[hd] = frontNode->data;
+        // }
+
+        topNode[hd] = frontNode->data;
+
+        if(frontNode->left){
+            q.push(make_pair(frontNode->left, hd-1));
+        }
+        if(frontNode->right){
+            q.push(make_pair(frontNode->right, hd+1));
+        }
+    }
+    
+    for(auto i : topNode){
+        ans.push_back(i.second);
+    }
+
+    return ans;
+}
+
+int main(){
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    vector<int> result = bottomView(root);
+
+    cout << "Bottom view of the given tree is : " << endl;
+    for(int i : result){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
