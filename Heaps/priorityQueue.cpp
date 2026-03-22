7cdc60032657642ccb93f08c9e6df4223087d5e4
sahilrai0201 <sahilatwork21@gmail.com>
2026-03-23 01:34:33 +0530

    Priority Queue

--- Heaps/priorityQueue.cpp
+++ Heaps/priorityQueue.cpp
@@ -7,19 +7,14 @@ int main(){
 
     //default max heap
     priority_queue<int> pq;
-
     pq.push(4);
     pq.push(2);
     pq.push(5);
     pq.push(3);
-
     cout << "Element at the top is : " << pq.top() << endl;
-
     pq.pop();
     cout << "Element at the top is : " << pq.top() << endl;
-
     cout << "Size : " << pq.size() << endl;
-
     if(pq.empty()){
         cout << "pq is empty" << endl;
     }
@@ -27,5 +22,24 @@ int main(){
         cout << "pq is not empty" << endl;
     }
 
+
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> minheap;
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(3);
+    cout << "Element at the top is : " << minheap.top() << endl;
+    minheap.pop();
+    cout << "Element at the top is : " << minheap.top() << endl;
+    cout << "Size : " << minheap.size() << endl;
+    if(minheap.empty()){
+        cout << "pq is empty" << endl;
+    }
+    else{
+        cout << "pq is not empty" << endl;
+    }
+
+    
     return 0;
 }
